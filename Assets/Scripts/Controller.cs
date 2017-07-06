3af0a95d87e48bf0911bc96aa2cbf1aa9190c3ee
QuynhDang <ndqpmbd@gmail.com>
2017-07-06 21:41:39 +0700

    update prototype

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    public Transform target;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnButtonClick(string buttonID) {
+        if (buttonID == ButtonID.FORWARD)
+        {
+            this.target.localPosition += Vector3.forward;
+        }
+        else if (buttonID == ButtonID.BACKWARD)
+        {
+            this.target.localPosition += Vector3.back;
+        }
+        else if (buttonID == ButtonID.LEFT)
+        {
+            this.target.localPosition += Vector3.left;
+        }
+        else if (buttonID == ButtonID.RIGHT)
+        {
+            this.target.localPosition += Vector3.right;
+        }
+        else if (buttonID == ButtonID.UP)
+        {
+            this.target.localPosition += Vector3.up;
+        }
+        else if (buttonID == ButtonID.DOWN)
+        {
+            this.target.localPosition += Vector3.down;
+        }
+    }
+}
+
+public class ButtonID {
+    public const string FORWARD = "forward";
+    public const string BACKWARD = "backward";
+    public const string LEFT = "left";
+    public const string RIGHT = "right";
+    public const string UP = "up";
+    public const string DOWN = "down";
+}
---
+++ Assets/Scripts/PuzzleRotation.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleRotation : MonoBehaviour {
+
+    public float rotationSpeed;
+    public float rotationRatio;
+
+    bool isRotating;
+    Vector3 mousePosition;
+    // Use this for initialization
+    void Start() {
+        this.isRotating = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.isRotating = true;
+            this.mousePosition = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            this.isRotating = false;
+        }
+        else {
+            if (isRotating) {
+                this.doRatation();
+
+            }
+
+        }
+
+    }
+
+    void doRatation() {
+        Vector3 oldMousePosition = mousePosition;
+        mousePosition = Input.mousePosition;
+        Vector3 rotationDelta = (mousePosition - oldMousePosition) * this.rotationRatio;
+        
+        this.transform.Rotate(rotationDelta  * this.rotationSpeed * Time.deltaTime);
+    }
+}
